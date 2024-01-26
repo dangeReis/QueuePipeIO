884eea1c006df6e711d1c3c93b9a977a8d5031b1
Russ Sivak <Russ.Sivak@moodys.com>
2024-01-25 20:53:50 -0500

    testing actions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='queue_bytes_io',
-    version='0.1.0',
+    version='0.1.1',
     description='A project that provides queue-based I/O functionality',
     packages=find_packages(),
     install_requires=[
