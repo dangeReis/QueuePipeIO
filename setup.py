e422312eac98b195c4357fdf429260bef7c092e7
Russ Sivak <Russ.Sivak@moodys.com>
2024-01-25 19:09:34 -0500

    updating setup

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='QueueBytesIO',
+    name='queueio',
     version='0.1.0',
-    url='https://github.com/dangeReis/QueueBytesIO',
-    description='A queue-based IO stream for Python',
-    packages=find_packages(),    
-    install_requires=['boto3', 'zstandard', 'tqdm'],
+    description='A project that provides queue-based I/O functionality',
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'zstandard',
+        'tqdm'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
